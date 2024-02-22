2ed635639e9bd0fb7e4d575f1e9b8a4e18660d7c
Ryan <ryanprayoga591@gmail.com>
2024-02-22 20:27:28 +0700

    Refactor code for improved performance

---
+++ pertemuan4/detail.php
@@ -0,0 +1,25 @@
+<?php
+ require 'function.php';
+  $mhs = query("SELECT * FROM mahasiswa WHERE id = {$_GET['id']}");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Detail Mahasiswa</title>
+</head>
+<body>
+  <h3>Detail Mahasiswa</h3>
+  <ul>
+    <li><img src="img/<?= $mhs['gambar']; ?>" width="100"></li>
+    <li>NRP: <?= $mhs['nrp']; ?></li>
+    <li>Nama: <?= $mhs['nama']; ?></li>
+    <li>Email: <?= $mhs['email']; ?></li>
+    <li>Jurusan: <?= $mhs['jurusan']; ?></li>
+    <li><a href="">ubah</a> | <a href="">hapus</a></li>
+    <li><a href="latihan2.php">Kembali ke daftar mahasiswa</a></li>
+  </ul>
+</body>
+</html>
---
+++ pertemuan4/function.php
@@ -0,0 +1,22 @@
+<?php
+
+function koneksi()
+{
+  return mysqli_connect('localhost','root','','pw_213040094');
+}
+
+function query($query)
+{
+  $conn = koneksi();
+  $result = mysqli_query($conn, $query);
+
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
